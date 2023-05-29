0845f3f113c8a24e28c5f32eb513c7947099d8e7
Juan Manuel Osorio Ch <75534146+JuanSh13@users.noreply.github.com>
2023-05-28 18:07:43 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,33 +1,58 @@
-var swiper = new Swiper('.swiper-container', {
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev'
-	},
-	slidesPerView: 1,
-	spaceBetween: 10,
-	// init: false,
-	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
-	},
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 6,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 10,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
+// // Initialize Swiper 
+// var swiper = new Swiper('.swiper-container', {
+// 	navigation: {
+// 	  nextEl: '.swiper-button-next',
+// 	  prevEl: '.swiper-button-prev'
+// 	},
+// 	slidesPerView: 1,
+// 	spaceBetween: 10,
+// 	// init: false,
+// 	pagination: {
+// 	  el: '.swiper-pagination',
+// 	  clickable: true,
+// 	},
 
   
-	breakpoints: {
-	  620: {
-		slidesPerView: 1,
-		spaceBetween: 20,
-	  },
-	  680: {
-		slidesPerView: 2,
-		spaceBetween: 40,
-	  },
-	  920: {
-		slidesPerView: 3,
-		spaceBetween: 40,
-	  },
-	  1240: {
-		slidesPerView: 4,
-		spaceBetween: 50,
-	  },
-	} 
-    });
+// 	breakpoints: {
+// 	  620: {
+// 		slidesPerView: 1,
+// 		spaceBetween: 20,
+// 	  },
+// 	  680: {
+// 		slidesPerView: 2,
+// 		spaceBetween: 40,
+// 	  },
+// 	  920: {
+// 		slidesPerView: 3,
+// 		spaceBetween: 40,
+// 	  },
+// 	  1240: {
+// 		slidesPerView: 4,
+// 		spaceBetween: 50,
+// 	  },
+// 	} 
+//     });
